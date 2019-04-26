099e90e09e8e46fe21630a0ba9d6d6037b4b8f1a
mm0rys <mmorysXQL@gmail.com>
2019-04-27 00:30:21 +0800

    Update spam-mail-double.py

--- spam-mail-double.py
+++ spam-mail-double.py
@@ -154,14 +154,14 @@ def do_dccnn(trainX, testX, trainY, testY):
     network = merge([branch21, branch22, branch23], mode='concat', axis=1)
     network = tf.expand_dims(network, 2)
     network = global_max_pool(network)
-    network = dropout(network, 0.8)
+    network = dropout(network, 0.4)
     network = fully_connected(network, 2, activation='softmax')
     network = regression(network, optimizer='adam', learning_rate=0.001,
                          loss='categorical_crossentropy', name='target')
     # Training
     model = tflearn.DNN(network, tensorboard_verbose=0)
     model.fit(trainX, trainY,
-              n_epoch=5, shuffle=True, validation_set=(testX, testY),
+              n_epoch=10, shuffle=True, validation_set=(testX, testY),
               show_metric=True, batch_size=100,run_id="spam")
 
 def do_cnn_wordbag(trainX, testX, trainY, testY):
